cb511afa909925d06acc2bf60d014ee707c824e8
Philip Henderson <philip.henderson@hotmail.com>
2022-10-10 15:57:42 -0400

    Added Enemy With Down screen Movement

--- Assets/[Scripts]/EnemyBehaviour.cs
+++ Assets/[Scripts]/EnemyBehaviour.cs
@@ -6,25 +6,51 @@ public class EnemyBehaviour : MonoBehaviour
 {
     public Boundary horizontalBoundary;
     public Boundary verticalBoundary;
-    public Boundary horizontalSpeedRange;
-
+    public Boundary screenBounds;
     public float horizontalSpeed;
+    public float verticalSpeed;
+    public SpriteRenderer spriteRenderer;
+    public Color randomColor;
+
     // Start is called before the first frame update
     void Start()
     {
-        var RandomXPosition = Random.Range(horizontalBoundary.min, horizontalBoundary.max);
-        var RandomYPosition = Random.Range(verticalBoundary.min, verticalBoundary.max);
-        horizontalSpeedRange.min = Random.Range(0.5f, 2.0f);
-        horizontalSpeedRange.max = Random.Range(2.0f, 6.0f);
-        horizontalSpeed = Random.Range(horizontalSpeedRange.min, horizontalSpeedRange.max);
-        transform.position = new Vector3(RandomYPosition, RandomYPosition, 0.0f);
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        ResetEnemy();
     }
 
     // Update is called once per frame
     void Update()
     {
+        Move();
+        CheckBounds();
+    }
+
+    private void Move()
+    {
         var horizontalLenth = horizontalBoundary.max - horizontalBoundary.min;
-        transform.position = new Vector3(Mathf.PingPong(Time.time * horizontalSpeed, horizontalLenth) - horizontalBoundary.max, 
-            transform.position.y, transform.position.z);
+        transform.position = new Vector3(Mathf.PingPong(Time.time * horizontalSpeed, horizontalLenth) - horizontalBoundary.max,
+            transform.position.y - verticalSpeed * Time.deltaTime, transform.position.z);
+    }
+    public void CheckBounds()
+    {
+        if (transform.position.y < screenBounds.min)
+        {
+            ResetEnemy();
+        }
+    }
+
+    public void ResetEnemy()
+    {
+        var RandomXPosition = Random.Range(horizontalBoundary.min, horizontalBoundary.max);
+        var RandomYPosition = Random.Range(verticalBoundary.min, verticalBoundary.max);
+        horizontalSpeed = Random.Range(1.0f, 6.0f);
+        verticalSpeed = Random.Range(1.0f, 3.0f);
+        transform.position = new Vector3(RandomYPosition, RandomYPosition, 0.0f);
+
+        List<Color> colorList = new List<Color>() { Color.red, Color.blue, Color.green, Color.white, Color.white, Color.white };
+
+        randomColor = colorList[Random.Range(0, 6)];
+        spriteRenderer.material.SetColor("_Color", randomColor);
     }
 }
