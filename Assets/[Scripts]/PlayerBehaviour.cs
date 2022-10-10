7a9ba26b5d9ad20c8eac7369693838b1c14e1273
Philip Henderson <philip.henderson@hotmail.com>
2022-10-10 12:13:18 -0400

    Added Touch Controls v1.0

--- Assets/[Scripts]/PlayerBehaviour.cs
+++ Assets/[Scripts]/PlayerBehaviour.cs
@@ -7,18 +7,36 @@ public class PlayerBehaviour : MonoBehaviour
     public float speed = 2/0f;
     public Boundary boundaries;
     public float verticalPosition;
+    public Camera camera;
+
+    void Start()
+    {
+        camera = Camera.main;
+    }
+
 
     void Update()
     {
+        //ConventionalInput();
         Move();
+        MobileInput();
+    }
+
+    public void MobileInput()
+    {
+        foreach (var touch in Input.touches)
+        {
+            transform.position = camera.ScreenToWorldPoint(touch.position);
+        }
+    }
+    public void ConventionalInput()
+    {
+        float x = Input.GetAxisRaw("Horizontal") * Time.deltaTime * speed;
+        transform.position += new Vector3(x, 0.0f, 0.0f);
     }
 
     public void Move()
     {
-        float x = Input.GetAxisRaw("Horizontal") * Time.deltaTime * speed;
-        float y = Input.GetAxisRaw("Vertical") * Time.deltaTime * speed;
-
-        transform.position += new Vector3(x, 0.0f, 0.0f);
         float clampledPosition = Mathf.Clamp(transform.position.x, boundaries.min, boundaries.max);
         transform.position = new Vector2(clampledPosition, verticalPosition);
     }
